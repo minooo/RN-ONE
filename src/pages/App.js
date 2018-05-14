2163f72dd2105dc1e080e941d7916de255ed0409
minooo <552594996@qq.com>
2018-05-14 09:11:29 +0800

    base

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Image } from "react-native";
 import { Title, Icon } from "@components";
 import s from "@style";
 
@@ -17,6 +17,7 @@ export default class App extends Component {
         <Icon name="left" size={40} color="#4F8EF7" />
         <Text style={[s.c_main, s.border_top]}>Hello, minooo!</Text>
         <View style={[s.w120, s.h40, s.bg_main, s.r10]} />
+        <Image source={require("@images/active_full.png")} />
         <Text
           style={[
             s.font10,
